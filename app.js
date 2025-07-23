bb1fbd174174ea3030c057a5512aca637a7fe1f8
Zunaira Mohsin <zunairamohsin76@gmail.com>
2025-07-23 20:50:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,409 @@
+
+let Student = {
+Name:"Zunaira",
+FatherName:"Mohsin",
+Age:17,
+Gender:"Female",
+}
+
+let Address ={
+Country:"Pakistan",
+City:"Karachi",
+Address:"Korangi J1",
+province: "Sindh",
+}
+
+let Education ={
+CurrentClass:"2nd year",
+college:"Goverment girls",
+field:"Computer science",
+Marks:72,
+hasClearedExam:"True",
+attendance:"Excellent",
+}
+
+let skills={
+Skills:["HTML,CSS,JavaScript"],
+html: true,
+ css: true,
+javascript: "intermediate",
+python: false,
+design: "basic",
+}
+
+let favoriteFood={
+name: ["Biryani","Burger","Fries","Pizza"],
+type: ["Desi","Fast Food"],
+ spicy: true,
+}
+
+let contectInf={
+phoneNumber:"03122781731",
+Email:"Zunairamohsin76@gmail.com",
+}
+
+let gameStats={
+ level: 10,
+  score: 1500,
+   lives: 3 
+} 
+
+let goals ={
+goals:["Learn JavaScript", "Build a portfolio", "Get a job"]
+} 
+
+let Subjects={
+subject1:"English",
+subject2:"urdu",
+subject3:"Math",
+subject4:"pysics",
+subject5:"computer",
+subject7:"Islamiat",
+}
+
+  let LaptopSpecs= {
+    brand: "Dell",
+    ram: "8GB",
+    processor: "i5",
+    storage: "256GB SSD"
+  }
+
+let SocialMedia ={
+    facebook: "zunaira.fb",
+    instagram: "@zunaira",
+    github: "zunaira-mohsin",
+  }
+
+  let CurrentsCourse={
+  course:"Web delopment" , 
+  Institude:"SMIT",
+  ClassTeacher:"Miss Minahil",
+  Timming:"9am to 11am",
+}
+
+  let WomenlongDress={
+Froks :{
+ type1 :"Long Froks",
+ type2: "Short Froks",
+},
+Maxis:{
+type1:"Havey MaXi",
+type2:"Simple Maxi",
+}
+}
+
+let WomenShirts={
+  kurti:{
+   type1:"Stylish",
+   type2:"Simple" ,
+  } 
+}
+
+let womenSendle={
+Sendle:{
+   type1:"Pumpi",
+   type2:"Sliper",
+}    
+}
+
+let EyeMeakup={
+ EyesMeakup:{
+type1:"Highlighter",
+type2:"lense",
+type3:"Kajal",
+ }
+ }  
+
+ let Lipstick={
+    Lipstick:{
+type1:"LipStick",
+type2:"Lips Pencil",
+type3:"Gloss",
+    }
+ } 
+
+ let BlushColor={
+    Blush:{
+   type1:"Pink",
+   type2:"Peach", 
+   type3: "Bronzer",
+    }
+ }
+
+ let FoundationBrands={
+    Foundation:{
+   type1:"Liquid",
+   type2:"Stick",
+    type3:"Powder", 
+    }
+ }
+
+ let MascaraBrand={
+Mascara:{
+type1:"Waterproof", 
+   type2:"Volume", 
+   type3:"Length",
+}
+ }
+
+ let EyelinerBrand={
+    Eyeliner:{
+        type1:"Gel",
+        type2: "Liquid",
+       type3: "Pencil",
+    }
+ }
+
+ let ConcealerBrand = {
+    Concealer: {
+        type1: "Cream",
+        type2: "Liquid",
+        type3: "Stick"
+    }
+}
+
+let PrimerBrand = {
+    Primer: {
+        type1: "Mattifying",
+        type2: "Hydrating",
+        type3: "Pore Filling"
+    }
+}
+
+let HighlighterBrand = {
+    Highlighter: {
+        type1: "Powder",
+        type2: "Liquid",
+        type3: "Stick"
+    }
+}
+
+let CompactBrand = {
+    Compact: {
+        type1: "Ivory",
+        type2: "Beige",
+        type3: "Tan"
+    }
+}
+
+let SettingSprayBrand = {
+    SettingSpray: {
+        type1: "Dewy",
+        type2: "Matte",
+        type3: "Natural"
+    }
+}
+
+let ContourBrand = {
+    Contour: {
+        type1: "Stick",
+        type2: "Powder",
+        type3: "Cream"
+    }
+}
+
+let BrowProductBrand = {
+    BrowProduct: {
+        type1: "Pencil",
+        type2: "Pomade",
+        type3: "Gel"
+    }
+}
+
+let NailPolishBrand = {
+    NailPolish: {
+        type1: "Red",
+        type2: "Nude",
+        type3: "Transparent"
+    }
+}
+
+let BrushesBrand = {
+    Brushes: {
+        type1: "Foundation Brush",
+        type2: "Blending Brush",
+        type3: "Angled Brush"
+    }
+}
+
+let MakeupRemoverBrand = {
+    MakeupRemover: {
+        type1: "Wipes",
+        type2: "Oil",
+        type3: "Micellar Water"
+    }
+}
+let BeautyBlenderBrand = {
+    BeautyBlender: {
+        type1: "Original",
+        type2: "Mini",
+        type3: "Micro"
+    }
+}
+
+let FaceWashBrand = {
+    FaceWash: {
+        type1: "Oil Control",
+        type2: "Hydrating",
+        type3: "Scrub"
+    }
+}
+
+let MoisturizerBrand = {
+    Moisturizer: {
+        type1: "Gel",
+        type2: "Cream",
+        type3: "Lotion"
+    }
+}
+
+let SerumBrand = {
+    Serum: {
+        type1: "Vitamin C",
+        type2: "Hyaluronic Acid",
+        type3: "Niacinamide"
+    }
+}
+
+let FaceMaskBrand = {
+    FaceMask: {
+        type1: "Clay",
+        type2: "Sheet",
+        type3: "Peel Off"
+    }
+}
+
+let SunscreenBrand = {
+    Sunscreen: {
+        type1: "SPF 30",
+        type2: "SPF 50",
+        type3: "Tinted"
+    }
+}
+
+let HairSprayBrand = {
+    HairSpray: {
+        type1: "Strong Hold",
+        type2: "Flexible",
+        type3: "Shine"
+    }
+}
+
+let ShampooBrand = {
+    Shampoo: {
+        type1: "Anti-Dandruff",
+        type2: "Volume",
+        type3: "Color-Protect"
+    }
+}
+
+let ConditionerBrand = {
+    Conditioner: {
+        type1: "Moisturizing",
+        type2: "Repair",
+        type3: "Smoothening"
+    }
+}
+
+let LipBalmBrand = {
+    LipBalm: {
+        type1: "Strawberry",
+        type2: "Mint",
+        type3: "Original"
+    }
+}
+
+let ShirtBrand = {
+    Shirt: {
+        type1: "Casual",
+        type2: "Formal",
+        type3: "Printed"
+    }
+}
+
+let TrouserBrand = {
+    Trouser: {
+        type1: "Jeans",
+        type2: "Chinos",
+        type3: "Cotton"
+    }
+}
+
+let KurtiBrand = {
+    Kurti: {
+        type1: "Embroidered",
+        type2: "Printed",
+        type3: "Plain"
+    }
+}
+
+let DupattaBrand = {
+    Dupatta: {
+        type1: "Chiffon",
+        type2: "Net",
+        type3: "Silk"
+    }
+}
+
+let FrockBrand = {
+    Frock: {
+        type1: "Long",
+        type2: "Short",
+        type3: "Party Wear"
+    }
+}
+
+let AbayaBrand = {
+    Abaya: {
+        type1: "Open",
+        type2: "Closed",
+        type3: "Embroidered"
+    }
+}
+
+let ScarfBrand = {
+    Scarf: {
+        type1: "Silk",
+        type2: "Cotton",
+        type3: "Wool"
+    }
+}
+
+let PantsBrand = {
+    Pants: {
+        type1: "Cigarette",
+        type2: "Palazzo",
+        type3: "Tights"
+    }
+}
+
+let TShirtBrand = {
+    TShirt: {
+        type1: "Graphic",
+        type2: "Plain",
+        type3: "Striped"
+    }
+}
+
+let SkirtBrand = {
+    Skirt: {
+        type1: "Mini",
+        type2: "Midi",
+        type3: "Maxi"
+    }
+}
+console.log(Student,CurrentsCourse,skills,ShampooBrand.Shampoo,ShirtBrand.Shirt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
